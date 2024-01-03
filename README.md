543f35007d1b2f09b18a8a8f70e735eaab66fe3e
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-01-03 13:31:11 -0700

    added SOAP provider, learn_soap, and consumer, gs-consuming-web-service-main, projects

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn_springboot
 from "Learning Spring Boot 3.0"
+
+
+## subproject learn_soap about providing a SOAP service  from https://spring.io/guides/gs/producing-web-service/#scratch
+
+### further about consuming from: https://spring.io/guides/gs/consuming-web-service/web
---
+++ gs-consuming-web-service-main/complete/consume.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#java -jar target/gs-consuming-web-service-0.1.0.jar
+java -jar target/consuming-web-service-complete-0.0.1-SNAPSHOT.jar 
+java -jar target/consuming-web-service-complete-0.0.1-SNAPSHOT.jar Spain
+java -jar target/consuming-web-service-complete-0.0.1-SNAPSHOT.jar Poland
+
